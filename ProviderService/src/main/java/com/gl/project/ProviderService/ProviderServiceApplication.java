6f5df2054a2970d28825e8abeb883582ffd0bbe0
varshitayerva <varshita.yerva@globallogic.com>
2026-04-07 23:41:08 +0530

    Added API Gateway and Eureka Server

--- ProviderService/src/main/java/com/gl/project/ProviderService/ProviderServiceApplication.java
+++ ProviderService/src/main/java/com/gl/project/ProviderService/ProviderServiceApplication.java
@@ -2,11 +2,12 @@ package com.gl.project.ProviderService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 
-
+@EnableDiscoveryClient
 public class ProviderServiceApplication {
 
 	public static void main(String[] args) {
