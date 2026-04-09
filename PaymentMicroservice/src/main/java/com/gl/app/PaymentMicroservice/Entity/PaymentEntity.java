d8a1ec9924f88f1d1ceb9a62b4b19e926241839f
J Sebastian <j.sebastian@globallogic.com>
2026-04-09 10:35:59 +0530

    Includes payments microservices files

--- PaymentMicroservice/src/main/java/com/gl/app/PaymentMicroservice/Entity/PaymentEntity.java
+++ PaymentMicroservice/src/main/java/com/gl/app/PaymentMicroservice/Entity/PaymentEntity.java
@@ -1,6 +1,7 @@
 package com.gl.app.PaymentMicroservice.Entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -33,5 +34,6 @@ public class PaymentEntity {
 
     @Enumerated(EnumType.STRING)
     private  PaymentStatus status;
+
 
 }
